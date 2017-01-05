203454b28851cbf261ebd8de52a4c2b72af86bf7
chetan <bairagisirfire@gmail.com>
2017-01-05 12:36:45 +0530

    First commit of Gpars actors

---
+++ src/main/groovy/com/nexthoughts/gparsActors/session2017/Exercise2.groovy
@@ -0,0 +1,46 @@
+package com.nexthoughts.gparsActors.session2017
+
+/**
+ * Created by chetan on 2/1/17.
+ */
+/*
+
+3.6. Exercise
+
+Now that we now how to send and receive a simple response from a given actor,
+lets modify the actor you built for the first exercise for
+instead of printing out the result, make it send back the result to you.
+
+You have to send three different list of numbers,
+ get back the results and sum them in the client
+  once you have received the partial results from the actor.
+
+The objective of this exercises is using promises or blocking calls to get results from a given actor.
+
+ */
+import static groovyx.gpars.actor.Actors.actor
+import groovyx.gpars.dataflow.Promise
+// you can use loop() to iterate, react() to receive messages, and reply()
+// to send a reply to the currently processed message.
+
+def printer = actor {
+    //loop() method call, which ensures that the actor doesn't stop after having processed the first message.
+
+    loop {
+        //react() method has a special semantics to allow actors to be detached
+        // from threads when no messages are available in
+        //their mailbox.
+                react { List<Integer> numbers ->
+           println numbers.sum()
+           reply numbers.sum()
+        }
+    }
+}
+//The sendAndPromise() method returns a Promise (aka Future) to the final reply
+// and so allows the caller to continue
+// its processing while the actor is handling the submitted message.
+Promise first = printer.sendAndPromise((1..4)) //10
+Integer second = printer.sendAndWait((4..8))//21
+Integer third = printer.sendAndWait((8..12))//50
+
+println  (first.get() + second + third) == 90
---
+++ src/main/groovy/com/nexthoughts/gparsActors/session2017/Simple-forwarding-NO-DSL.groovy
@@ -0,0 +1,62 @@
+package com.nexthoughts.gparsActors.session2017
+
+/**
+ * Created by chetan on 24/11/16.
+ */
+import groovyx.gpars.actor.DefaultActor
+/*
+Suppose you want to send a message to a given actor and then get
+ the response back to send it to another actor.
+ Well good news is that you can set where the response should be sent.
+ */
+class WithId extends DefaultActor {
+    String id
+    String toString() { return this.id }
+}
+
+class Bank extends WithId {
+    void act() {
+        loop {
+//            println 'yyyyyy'
+//first it comes here  calculates 1000, 2000 then goes to branch
+            react { Integer amount ->
+                println Thread.currentThread().name
+                reply "Amount $amount has been stored in -- ${sender} --"
+                //stop()
+
+            }
+        }
+    }
+}
+class BankBranch extends WithId {
+    void act() {
+        loop {
+//            println 'nnnn'
+            //as per previous examples we can guess this is the sender or intiatoir who may have send request for cash
+            // and the reciever is the bank who will listen the request and send the money
+            react { String message ->
+                println sender
+                println message
+            }
+        }
+    }
+}
+/*
+When sending a message, a different actor can be specified as the sender so that potential replies to the message will
+be forwarded to the specified actor and not to the actual originator.
+ */
+//we start the bank and the branch and created 2 instance of branch which is the sender
+final branch1 = new BankBranch(id: '0001').start()
+final branch2 = new BankBranch(id: '0002').start()
+//branch1.join()
+final bank = new Bank(id: 'abcd').start()
+// public final <T> MessageStream send(T message, MessageStream replyTo) {
+//so as per definition bank has taken the request and sending money to branches
+//send(T message, MessageStream replyTo) {
+bank.send(1000, branch1)//100 rupees send ho gaye  ////Specify an actor to send replies to
+bank.send(2000, branch2)
+//Actors provide a join() method to allow callers to wait for the actor to terminate.
+bank.join()
+//bank.stop()
+//
+//bank.sendAndWait('mmmm')
